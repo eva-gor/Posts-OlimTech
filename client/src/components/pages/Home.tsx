80630b54f34519c2aca089507fb96ef960475fcf
eva-gor <evagor82@gmail.com>
2023-09-23 15:54:11 +0300

    +paginator

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,24 +1,25 @@
-import { Box, Grid } from "@mui/material";
-import React, { useEffect, useRef, useState } from "react";
+import { Box, Grid, Pagination } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import HeaderForm from "../forms/HeaderForm";
 import AddUpdatePostForm from "../forms/AddUpdatePostForm";
-import { useDispatchCode, useSelectorPostsByPage } from "../../hooks/hooks";
-import PostType from "../model/PostType";
-import { postService } from "../../config/service-config";
-import GetPostsByPageType from "../model/GetPostsByPageType";
+import { useSelectorPostsByPage } from "../../hooks/hooks";
 import ThumbnailCardForm from "../forms/ThumbnailCardForm";
-import Pagination from '@mui/material/Pagination';
-
 
 const Home: React.FC = () => {
     const [pkRes, setPkRes] = useState<{ page: number, keyword: string }>({ page: 1, keyword: '' });
     const [openAddPostForm, setOpenAddPostForm] = useState<boolean>(false);
     const allPosts = useSelectorPostsByPage(pkRes.page, pkRes.keyword);
+    useEffect(()=>{
+        window.scrollTo(0, 0);
+    },[pkRes.page]);
+    useEffect(()=>{
+        setPkRes({...pkRes, page: 1});
+    }, [pkRes.keyword])
 
     function getThumbnails() {
         return Array.isArray(allPosts.result) ? allPosts.result.map(post => <Grid item xs={12} lg={6} xl={4}><ThumbnailCardForm post={post} /></Grid>) : [];
     }
-    return <Box width='100%' sx={{ textAlign: "center" }} key='hpBox'>
+    return <Box width='100%' sx={{ textAlign: "center" }} key='hpBox' >
         <HeaderForm getKeyword={keyword => setPkRes({ ...pkRes, keyword })} setOpenAddPostForm={() => setOpenAddPostForm(true)} />
         <AddUpdatePostForm openDialog={openAddPostForm} goBack={() => setOpenAddPostForm(false)} />
         {getThumbnails() && getThumbnails().length === 0 ? <label style={{ textAlign: 'center', color: 'lightgrey', fontSize: '3rem', marginTop: '10vh', fontFamily: 'cursive' }}>No items</label> :
@@ -26,8 +27,9 @@ const Home: React.FC = () => {
                 {getThumbnails()}
             </Grid>
         }
-
-        {/* <Pagination count={postsInfo.totalPages} color="secondary" />   */}
+        <div style={{ width: '100%', zIndex: 100, position: 'fixed',  bottom: '0px', display: 'flex', justifyContent: 'center', backgroundColor: 'white'}}>
+            <Pagination count={allPosts.totalPages} color="secondary" page={pkRes.page} onChange={(event: React.ChangeEvent<any>, page: number) => setPkRes({ ...pkRes, page })} />
+        </div>
     </Box>
 }
 export default Home;
